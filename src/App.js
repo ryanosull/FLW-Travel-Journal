b8fc7d0da1f184f98dd18a039fd0d04b41fca0f5
Ryan <wrosullivan88@gmail.com>
2022-11-02 17:20:04 -0500

    nav and logo asset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import HomesContainer from "./HomesContainer";
+import Nav from "./Nav";
 
 // import './db.json';
 
@@ -10,15 +11,16 @@ function App() {
   //   alert("talkin' to you, dorks")
   // }
 
-  const buttonClick = () => {
-    alert("talkin' to you, dorks")
-  }
+  // const buttonClick = () => {
+  //   alert("talkin' to you, dorks")
+  // }
 
 
   return (
     <div className="App">
-      <h3>hello dorks</h3>
-      <button onClick={buttonClick}>click me bby</button>
+      <Nav />
+      {/* <h3>hello dorks</h3>
+      <button onClick={buttonClick}>click me bby</button> */}
       <br />
       <hr />
       <br />
