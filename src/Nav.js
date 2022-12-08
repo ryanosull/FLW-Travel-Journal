f78c8328b1149daee242214ac07606ee12e5668d
Ryan <wrosullivan88@gmail.com>
2022-12-08 11:00:06 -0600

    git init on FLW

--- src/Nav.js
+++ src/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
 	return (
         <div >
             <div className="navSegment" >
-                <h2 className="navText">A Travel Guide to Frank Lloyd Wright Homes</h2> 
+                <h2 className="navText" >A Travel Guide to Frank Lloyd Wright Homes</h2> 
             </div>
         </div>
 	);
