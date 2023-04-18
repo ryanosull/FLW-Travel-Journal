3c3cf60351fde6892e26aac5eaf428bbee66dc1e
William Ryan OSullivan <wrosullivan@gmail.com>
2023-04-18 12:53:08 -0500

    input attributes

--- src/NewHomeForm.js
+++ src/NewHomeForm.js
@@ -56,20 +56,20 @@ function NewHomeForm ({homes, setHomes}) {
             <form onSubmit={handleSubmitHome} id="newHomeForm" >
 
                 <input onChange={(e) => setName(e.target.value)} 
-                type="text" name="name" value={name} className="formInputs" placeholder="Name" />
+                type="text" name="name" value={name} className="formInputs" placeholder="Name" required />
 
                 <textarea onChange={(e) => setDescription(e.target.value)}
-                type="text" name="description" value={description} className="formInputs" maxLength="500" placeholder="Description" />
+                type="text" name="description" value={description} className="formInputs" maxLength="500" placeholder="Description" required />
 
                 <input onChange={(e) => setCity(e.target.value)}
-                type="text" name="city" value={city} className="formInputs" placeholder="City" />
+                type="text" name="city" value={city} className="formInputs" placeholder="City" required />
 
                 <input onChange={(e) => setState(e.target.value)} className="formInputs"
-                type="text" name="state" value={state} placeholder="State" />
+                type="text" name="state" value={state} placeholder="State" required />
 
                 <input onChange={(e) => setCompleted(e.target.value)} className="formInputs"
-                type="text" name="completed" value={completed}
-                minLength="4" maxLength="4" min="0" placeholder="Year Completed" />
+                type="number" name="completed" value={completed}
+                minLength="4" maxLength="4" min="1867" max="1959" placeholder="Year Completed" />
 
                 <select value={usonian} onChange={(e) => setUsonian(e.target.value)} className="usonianDropdown" placeholder="Usonian" required >
                     <option className="usonianDropdown" >Usonian?</option>
@@ -78,7 +78,7 @@ function NewHomeForm ({homes, setHomes}) {
                 </select>
 
                 <input onChange={(e) => setRating(e.target.value)} className="formInputs"
-                type="number" name="rating" value={rating} min="0" max="10" maxLength="1" placeholder="Rating" />
+                type="number" name="rating" value={rating} min="0" max="10" maxLength="1" placeholder="Rating" required />
 
                 <input onChange={(e) => setImage(e.target.value)} className="formInputs"
                 type="text" name="image" value={image} placeholder="Image URL" />
