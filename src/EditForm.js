e5c092a9bbdc4171556b681416448dd90a030f42
William Ryan OSullivan <wrosullivan@gmail.com>
2023-04-22 19:25:42 -0500

    new approach didn't work. edit button always flips card back ove. leaving it alone for the night. oh and fuck you late night commits AAAAAHHHHHHHHHHHHHHHHH

---
+++ src/EditForm.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function EditForm () {
+
+
+
+
+
+
+
+
+
+
+    return (
+        <div>
+            <h1>this is a fucking test</h1>
+        </div>
+    );
+};
+
+export default EditForm;
