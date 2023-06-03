f773a3589bf498c6b0e1413a42085c8cdc4c85a2
William Ryan OSullivan <wrosullivan@gmail.com>
2023-06-03 18:41:47 -0500

    some styling

--- src/Sort.js
+++ src/Sort.js
@@ -6,8 +6,8 @@ function Sort ({changeSort}) {
         <div className="sortFilterText">Sort by:&nbsp;&nbsp;&nbsp;
             <select className="drop-down" onChange={(e) => changeSort(e.target.value)}>
                 <option value="None">None</option>
-                <option value="Oldest">Oldest first</option>
-                <option value="Newest">Most recent first</option>
+                <option value="Oldest">Earliest constructed</option>
+                <option value="Newest">Most recently constructed</option>
             </select>
         </div>
     )
