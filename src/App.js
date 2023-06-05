7e014ad47feb476e0cd6703c50cb5c5aa8e54fef
William Ryan OSullivan <wrosullivan@gmail.com>
2023-06-04 20:05:07 -0500

    all done here

--- src/App.js
+++ src/App.js
@@ -9,16 +9,21 @@ import guggenheim from "../src/assets/guggenheim.png"
 //
 
 
+
+//endpoint variables
 const baseUrl = "http://localhost:8000/"
 const homesUrl = baseUrl + "flwHomes/"
 
 function App() {
 
+  //initial state for fetch
   const [homes, setHomes] = useState([])
 
+  //state for filter and sort functionality
   const [filter, setFilter] = useState("All")
   const [sort, setSort] = useState("None")
 
+  //initial fetch
   useEffect (() => {
     fetch(homesUrl)
     .then(resp => resp.json())
@@ -26,15 +31,16 @@ function App() {
   }, [])
 
 
-  function changeFilter (newFilter) {
+  function changeFilter (newFilter) { //helper function
       setFilter(newFilter)
   }
 
 
-  function changeSort (newSort) {
+  function changeSort (newSort) { //helper function
     setSort(newSort)
   }
 
+  //filter
   function filteredHomes () {
     if (filter === 'Usonian')
       return homes.filter(home => home.usonian)
@@ -43,7 +49,7 @@ function App() {
     else return [...homes]
   }
 
-
+  //sorting functionality
   function sortHomes () {
 
     let sortedHomes = filteredHomes()
@@ -51,11 +57,51 @@ function App() {
     if (sort === "Oldest") {
       return sortedHomes.sort((year1, year2) => year1.completed - year2.completed)
     }
+
     else if (sort === "Newest" ) {
-    
       return sortedHomes.sort((year1, year2) => year2.completed - year1.completed)
     }
-    else return sortedHomes
+
+    else if (sort === "Highest") {
+      return sortedHomes.sort((rating1, rating2) => rating2.rating - rating1.rating)
+    }
+
+    else if (sort === "Lowest") {
+      return sortedHomes.sort((rating1, rating2) => rating1.rating - rating2.rating)
+    }
+
+    //convert the date strings to Date objects using the Date constructor. then, compare the Date objects using the comparison operators.
+
+    else if (sort === "First") {
+
+      function compareFirstDates(date1, date2){
+
+        const dateObj1 = new Date(date1.date);
+        const dateObj2 = new Date(date2.date);
+
+        if (dateObj1 < dateObj2) {
+          return -1 
+        }
+      }
+      return sortedHomes.sort(compareFirstDates)
+      
+    }
+
+    else if (sort === "Last") {
+
+      function compareLastDates(date1, date2){
+
+        const dateObj1 = new Date(date1.date);
+        const dateObj2 = new Date(date2.date);
+
+        if (dateObj2 < dateObj1) {
+          return -1 
+        }
+      }
+      return sortedHomes.sort(compareLastDates)
+    }
+    
+    else return sortedHomes //else do nothing
   }
 
   function handleDelete(id) {
@@ -64,20 +110,24 @@ function App() {
   }
 
 
+
   return (
     <div className="App">
 
       <Header />
-      
+  
       <NewHomeForm homes={homes} setHomes={setHomes} />
+
       <div id="sortFilter">
         <Sort changeSort={changeSort} />
+
         <Filter changeFilter = {changeFilter} />
       </div>
+
       <HomesContainer homes={sortHomes()} setHomes={setHomes} handleDelete={handleDelete}/>
 
       <div id="guggDiv">
-        <img id="gugg" src={guggenheim} alt="oh nooooo"/>
+        <img id="gugg" src={guggenheim} alt="oh nooooo!"/>
       </div>
 
     </div>
@@ -86,5 +136,7 @@ function App() {
 
 export default App;
 
+
+//Environment setup ↓
 //In a new terminal: `` json-server db.json --watch --port 8000 ``
 //In a seperate terminal: `` npm install `` then `` npm start ``
